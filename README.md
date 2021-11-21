f0498a29192df78cbdaa56d3aa7667108a33ded1
Johan Hage <johan.hag3@gmail.com>
2021-11-21 16:58:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Johan Hage
+
+## A website made by me
+
+This is a simple website made by me, Johan Hage, just as a small showcase of what I can do.
---
+++ src/routes/index.svelte
@@ -0,0 +1,40 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Johan Hage</title>
+</svelte:head>
+
+<section>
+	<h1 class="title">
+		Johan Hage
+	</h1>
+
+	<h2>
+		Software engineer - Web developer - Human
+	</h2>
+
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.title {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+</style>
